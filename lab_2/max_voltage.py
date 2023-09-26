ca66341ee5e59a462d912f3b6f39f26c95043a16
Mathieu Marquis <mamar828@ulaval.ca>
2023-09-26 09:48:34 -0400

    max_voltage

---
+++ lab_2/max_voltage.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+
+# Max voltage calculation
+
+resistances = np.array([
+    12, 270, 560, 100e3, 1e6
+])
+
+max_wattage = 1/40
+
+max_voltage = np.sqrt(max_wattage*resistances)
+
+np.set_printoptions(suppress=True)
+print("Max voltage:")
+print(np.stack((resistances, max_voltage), axis=1))
