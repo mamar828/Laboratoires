4cfaf8aff39b77998fe3918e6b7d10146782cf1b
Mathieu Marquis <mamar828@ulaval.ca>
2023-10-14 20:56:09 -0400

    something

--- lab_3/graph_maker.py
+++ lab_3/graph_maker.py
@@ -50,29 +50,30 @@ def make_figures_for_parts_abce():
     plot_files = [
         ["lab_3/data/part_3_a.lvm", "lab_3/graphs/part_3_a.png", {
             "title": "Courant en fonction de la différence de potentiel aux bornes \nd'une résistance de 1.2 kΩ",
-            "xlabel": "Différence de potentiel $∆V$ (V)",
-            "ylabel": "Courant $I$ (A)"
+            "xlabel": "Différence de potentiel $∆V$ [V]",
+            "ylabel": "Courant $I$ [A]"
         }],
         ["lab_3/data/part_3_b.lvm", "lab_3/graphs/part_3_b.png", {
             "title": "Courant en fonction de la différence de potentiel aux bornes \nd'un condensateur de 1 μF",
-            "xlabel": "Différence de potentiel $∆V$ (V)",
-            "ylabel": "Courant $I$ (A)"
+            "xlabel": "Différence de potentiel $∆V$ [V]",
+            "ylabel": "Courant $I$ [A]"
         }],
         ["lab_3/data/part_3_c.lvm", "lab_3/graphs/part_3_c.png", {
             "title": "Courant en fonction de la différence de potentiel aux bornes \nd'une bobine de 1 mH",
-            "xlabel": "Différence de potentiel $∆V$ (V)",
-            "ylabel": "Courant $I$ (A)"
+            "xlabel": "Différence de potentiel $∆V$ [V]",
+            "ylabel": "Courant $I$ [A]"
         }],
         ["lab_3/data/part_3_e.lvm", "lab_3/graphs/part_3_e.png", {
             "title": ("Courant en fonction de la différence de potentiel aux bornes \n" + 
                       "d'une diode Zener branchée en sens inverse"),
-            "xlabel": "Différence de potentiel $∆V$ (V)",
-            "ylabel": "Courant $I$ (A)"
+            "xlabel": "Différence de potentiel $∆V$ [V]",
+            "ylabel": "Courant $I$ [A]"
         }]
     ]
     for data, save_path, params in plot_files:
         plot_graph(read_lvm(data), params=params)
         save_figure(save_path, show=True)
+
 # make_figures_for_parts_abce()
 
 def make_figure_for_part_d():
@@ -81,13 +82,12 @@ def make_figure_for_part_d():
     global_array = np.concatenate(((zero_to_six * (-1))[::-1], zero_to_one[1:,:]))
     np.save("lab_3/data/concatenated_part_4.npy", global_array)
     params = {
-        "title": "Courant en fonction de la différence de potentiel aux bornes\nd'une diode standard",
-        "xlabel": "Différence de potentiel $∆V$ (V)", 
-        "ylabel": "Courant $I$ (A)"
+        "title": "Courant en fonction de la différence de\npotentiel aux bornes d'une diode standard",
+        "xlabel": "Différence de potentiel $∆V$ [V]", 
+        "ylabel": "Courant $I$ [A]"
     }
     plot_graph(global_array, params)
-    plt.show()
-    # save_figure("lab_3/graphs/part_3_d.png", show=True)
+    save_figure("lab_3/graphs/part_3_d.png", show=True)
 
 # make_figure_for_part_d()
 
@@ -95,19 +95,19 @@ def make_figures_for_part_4():
     arrays = read_lvm("lab_3/data/part_4_ok_17.lvm")
     params = {
         "title": r"Courant $i_{ce}$ en fonction de la tension $v_{ce}$ pour différentes tensions $v_{be}$",
-        "xlabel": r"Différence de potentiel $v_{ce}$ (V)", 
-        "ylabel": r"Courant $i_{ce}$ (A)"
+        "xlabel": r"Différence de potentiel $v_{ce}$ [V]", 
+        "ylabel": r"Courant $i_{ce}$ [A]"
     }
     fig = plt.figure()
     ax = fig.add_subplot(1,1,1)
-    ax.plot(arrays[0][:,0], arrays[0][:,1], "g-", markersize=2, label=r"Tension $v_{be}=0.0$ V")
-    ax.plot(arrays[1][:,0], arrays[1][:,1], "r-", markersize=2, label=r"Tension $v_{be}=0.2$ V")
-    ax.plot(arrays[2][:,0], arrays[2][:,1], "b-", markersize=2, label=r"Tension $v_{be}=0.4$ V")
-    ax.plot(arrays[3][:,0], arrays[3][:,1], "y-", markersize=2, label=r"Tension $v_{be}=0.6$ V")
-    ax.plot(arrays[4][:,0], arrays[4][:,1], "m-", markersize=2, label=r"Tension $v_{be}=0.8$ V")
-    ax.plot(arrays[5][:,0], arrays[5][:,1], "k-", markersize=2, label=r"Tension $v_{be}=1.0$ V")
+    ax.plot(arrays[0][:,0], arrays[0][:,1], "go", markersize=2, label=r"Tension $v_{be}=0.0$ V")
+    ax.plot(arrays[1][:,0], arrays[1][:,1], "ro", markersize=2, label=r"Tension $v_{be}=0.2$ V")
+    ax.plot(arrays[2][:,0], arrays[2][:,1], "bo", markersize=2, label=r"Tension $v_{be}=0.4$ V")
+    ax.plot(arrays[3][:,0], arrays[3][:,1], "yo", markersize=2, label=r"Tension $v_{be}=0.6$ V")
+    ax.plot(arrays[4][:,0], arrays[4][:,1], "mo", markersize=2, label=r"Tension $v_{be}=0.8$ V")
+    ax.plot(arrays[5][:,0], arrays[5][:,1], "ko", markersize=2, label=r"Tension $v_{be}=1.0$ V")
     try:
-        ax.plot(arrays[6][:,0], arrays[6][:,1], "c-", markersize=2, label=r"Tension $v_{be}=1.2$ V")
+        ax.plot(arrays[6][:,0], arrays[6][:,1], "co", markersize=2, label=r"Tension $v_{be}=1.2$ V")
     except:
         pass
     ax.set_title(params["title"])
@@ -131,23 +131,22 @@ def make_figures_for_part_4():
     print(f"  0mV std: {np.std(arrays[0][:,1])}")
     print(f"200mV std: {np.std(arrays[1][:,1])}")
     print(f"400mV std: {np.std(arrays[2][1:,1])}")
-    # save_figure("lab_3/graphs/part_4.png", show=True)
-    plt.show()
+    save_figure("lab_3/graphs/part_4.png", show=True)
 
 make_figures_for_part_4()
 
 def make_figure_for_dvdi():
     array = np.load("lab_3/data/concatenated_part_4.npy")
     diff_array = np.diff(array, n=1, axis=0)
-    plotted_array = np.stack((array[:-1,0], np.abs(diff_array[:,0]/diff_array[:,1])), axis=1)
+    plotted_array = np.stack((array[:-1,0], diff_array[:,0]/diff_array[:,1]), axis=1)
     params = {
         "title": ("Résistance dynamique $R_D$ en fonction de la différence\n" + 
                   "de potentiel aux bornes d'une diode standard"),
-        "xlabel": "Différence de potentiel $∆V$ (V)", 
-        "ylabel": "Résistance dynamique $R_D$ (Ω)"
+        "xlabel": "Différence de potentiel $∆V$ [V]", 
+        "ylabel": "Résistance dynamique $R_D$ [Ω]"
     }
     plot_graph(plotted_array, params)
-    save_figure("lab_3/graphs/dynamic_resistance_abs.png", show=True)
+    save_figure("lab_3/graphs/dynamic_resistance.png", show=True)
 
 # make_figure_for_dvdi()
 
@@ -156,8 +155,8 @@ def make_figure_for_fitted_diode():
     params = {
         "title": ("Courant en fonction de la différence de potentiel aux bornes \nd'une diode standard avec un " + 
                   "ajustement de l'équation de Shockley"),
-        "xlabel": "Différence de potentiel $∆V$ (V)", 
-        "ylabel": "Courant $I$ (A)"
+        "xlabel": "Différence de potentiel $∆V$ [V]", 
+        "ylabel": "Courant $I$ [A]"
     }
 
     def shockley_equation(v, i_0, v_0):
@@ -165,6 +164,8 @@ def make_figure_for_fitted_diode():
 
     i_0, v_0 = scipy.optimize.curve_fit(shockley_equation, array[:,0], array[:,1])[0]
     plot_graph(array, params)
+    print(f"i_0: {i_0}")
+    print(f"v_0: {v_0}")
 
     x_space = np.linspace(-6,1, 350)
     plt.plot(x_space, shockley_equation(x_space, i_0, v_0), "b-", markersize=2)
@@ -172,7 +173,8 @@ def make_figure_for_fitted_diode():
 
     residue = shockley_equation(array[:,0], i_0, v_0) - array[:,1]
     plot_graph(np.stack((array[:,0], residue), axis=1))
-    plt.title(f"residue stddev: {np.std(residue)}")
+    print(f"residue stddev: {np.std(residue)}")
+    print(f"residue mean  : {np.mean(residue)}")
     save_figure("lab_3/graphs/shockley_residue.png", show=True)
     
 # make_figure_for_fitted_diode()
