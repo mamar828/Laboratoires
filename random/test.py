d8b9e3a85e1417037ac53b8251727a0232a26bae
Mathieu Marquis <mamar828@ulaval.ca>
2023-10-21 15:23:52 -0400

    lab_4 setup

--- random/test.py
+++ random/test.py
@@ -1,11 +1,29 @@
 import numpy as np
+import scipy
+import matplotlib.pyplot as plt
 
 
-a = np.array([
-    [1,2],
-    [3,3],
-    [6,4],
-    [10,6]
-])
 
-print(np.diff(a, n=1, axis=0))
+
+
+x_space = np.linspace(-6,1,36)
+i_0 = 10**(-8)
+v_0 = 0.15
+
+y_space = i_0*(np.e**((x_space+(np.random.random(len(x_space)))/15)/v_0)-1)
+
+plt.plot(x_space, y_space, "go", markersize=2)
+# plt.show()
+
+
+def shockley_equation(v, i_0, v_0):
+    return i_0*(np.e**(v/v_0)-1)
+
+i_0, v_0 = scipy.optimize.curve_fit(shockley_equation, x_space, y_space)[0]
+print(i_0, v_0)
+
+x_shockley = np.linspace(-6,1,1000)
+plt.plot(x_shockley, shockley_equation(x_shockley, i_0, v_0), "b-")
+plt.xlabel(r"tension $v_{be}$")
+plt.ylabel(r"courant $i_{ce}$")
+plt.show()
