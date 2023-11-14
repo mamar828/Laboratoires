c4047940b8bd78125c193e31939adefd89b0775d
Mathieu Marquis <mamar828@ulaval.ca>
2023-11-14 09:26:07 -0500

    graphing with uncertainties + cleanup

--- lab_5/graph_maker.py
+++ lab_5/graph_maker.py
@@ -45,52 +45,89 @@ def save_figure(file_path: str, show: bool=False):
     if show:
         plt.show(block=True)
 
+
+def get_dissipated_power(filename: str) -> tuple:
+    data = read_lvm(filename)
+    # plt.hist(data[:,0])
+    # plt.show()
+    resistance = np.mean(data[:,0]), np.std(data[:,0])
+    potential  = scipy.stats.mode(data[:,1]).mode[0], 0.0005
+    power = (
+        potential[0]**2 / resistance[0], 
+        (potential[1] / potential[0] * 2 + resistance[1] / resistance[0]) * potential[0]**2 / resistance[0]
+    )
+    return resistance, potential, power
+
+
+# print(get_dissipated_power("lab_5/data_new/capacitor/part_4_6.lvm"))
+
+
+
 def make_power_figure():
-    # Only resistance curve
-    resistance_data = []
-    # resistance_data.append(read_lvm("lab_5/data/lab_5_data.lvm"))
-    for i in range(1,24):
-        resistance_data.append(read_lvm(f"lab_5/data_new/resistance/part_1_{i}.lvm"))
-    
-    r_global_array = np.stack((resistance_data), axis=0)
-    r_dissipated_power = r_global_array[:,:,1]**2 / r_global_array[:,:,0]
-    r_plotted_array = np.stack((np.mean(r_global_array[:,:,0], axis=1), np.mean(r_dissipated_power, axis=1)), axis=1)
+    # Resistance curve
+    resistance_values = []
+    resistance_powers = []
+    for part_no in range(1,24):
+        resistance, potential, power = get_dissipated_power(f"lab_5/data_new/resistance/part_1_{part_no}.lvm")
+        resistance_values.append(resistance)
+        resistance_powers.append(power)
 
-    # Adding resistance + capacitor curve
-    capacitor_data = []
-    for i in range(1,23):
-        capacitor_data.append(read_lvm(f"lab_5/data_new/capacitor_low/part_4_{i}.lvm"))
+    # Resistance curve
+    capacitor_values = []
+    capacitor_powers = []
+    for part_no in range(1,23):
+        resistance, potential, power = get_dissipated_power(f"lab_5/data_new/capacitor/part_4_{part_no}.lvm")
+        capacitor_values.append(resistance)
+        capacitor_powers.append(power)
 
-    c_global_array = np.stack((capacitor_data), axis=0)
-    c_dissipated_power = c_global_array[:,:,1]**2 / c_global_array[:,:,0]
-    c_plotted_array = np.stack((np.mean(c_global_array[:,:,0], axis=1), np.mean(c_dissipated_power, axis=1)), axis=1)
-    
-    # Plotting and setting the parameters
-    for array, markertype, label in [[r_plotted_array, "go", "Circuit de la Figure 1"],
-                                     [c_plotted_array, "mo", "Circuit de la Figure 2"]]:
-        plt.plot(array[:,0], array[:,1], markertype, label=label, markersize=2)
-    
-    def power_equation(R, a, b, h, k, d):
-        return a * (10**(b * (R-d)) - h)**2 + k
-    
-    def power_equation_1(R, a, h):
-        return a * (10**(R) - h)**2
-    
-    ordered_i = np.argsort(r_plotted_array[:,0])
-    r_plotted_array_ordered = np.stack((r_plotted_array[:,0][ordered_i], r_plotted_array[:,1][ordered_i]), axis=1)
+    r_v_array = np.array(resistance_values)
+    r_p_array = np.array(resistance_powers)
+    c_v_array = np.array(capacitor_values)
+    c_p_array = np.array(capacitor_powers)
 
-    # a, h = scipy.optimize.curve_fit(power_equation_1, r_plotted_array_ordered[:,0], r_plotted_array_ordered[:,1])[0]
-    a_1, b_1, h_1, k_1, d_1 = scipy.optimize.curve_fit(power_equation, r_plotted_array_ordered[:,0], r_plotted_array_ordered[:,1], p0=[])[0]
-    # a_2, b_2, h_2, k_2, d_2 = scipy.optimize.curve_fit(power_equation, c_plotted_array[:,0], c_plotted_array[:,1])[0]
-    
-    # print(r_plotted_array)
-    # print(r_plotted_array_ordered)
+    plt.errorbar(r_v_array[:,0], r_p_array[:,0], xerr=r_v_array[:,1], yerr=r_p_array[:,1], markersize=2, fmt="go",
+                 label="Circuit 1")
+    plt.errorbar(c_v_array[:,0], c_p_array[:,0], xerr=c_v_array[:,1], yerr=c_p_array[:,1], markersize=2, fmt="mo",
+                 label="Circuit 2")
+
+    def power_equation(R_charge, R_source, V_source):
+        return V_source**2 / (R_charge + R_source)**2 * R_charge
+
+    R_source_1, V_source_1 = scipy.optimize.curve_fit(power_equation, r_v_array[:,0], r_p_array[:,0])[0]
+    R_source_2, V_source_2 = scipy.optimize.curve_fit(power_equation, c_v_array[:,0], c_p_array[:,0])[0]
 
     x_space = np.linspace(14, 200, 500)
-    # plt.plot(x_space, power_equation_1(x_space, a, h), "g-")
-    plt.plot(x_space, power_equation(x_space, a_1, b_1, h_1, k_1, d_1), "g-")
-    # plt.plot(x_space, power_equation(x_space, a_2, b_2, h_2, k_2, d_2), "m-")
+    plt.plot(x_space, power_equation(x_space, R_source_1, V_source_1), "g-", linewidth=1)
+    plt.plot(x_space, power_equation(x_space, R_source_2, V_source_2), "m-", linewidth=1)
 
+    plt.title("Puissance moyenne dissipée [W] par la charge en fonction de la résistance [$\Omega$]")
+    plt.xlabel("Résistance totale des composantes du circuit [$\Omega$]")
+    plt.ylabel("Puissance moyenne dissipée [W]")
+    # plt.xscale("log")
+    plt.legend(loc="upper right")
+    # plt.show()
+
+
+make_power_figure()
+
+
+
+
+def make_filix_figure():
+    for part_no, markertype in zip([5,6,7,8,9], ["g<", "m<", "b<", "k<", "y<"]):
+        data = []
+        n = 1
+        while True:
+            try:
+                data.append(read_lvm(f"lab_5/data_new/varying_capacity/part_{part_no}_{n}.lvm"))
+                n += 1
+            except: break
+
+        global_array = np.stack((data), axis=0)
+        dissipated_power = global_array[:,:,1]**2 / global_array[:,:,0]
+        plotted_array = np.stack((np.mean(global_array[:,:,0], axis=1), np.mean(dissipated_power, axis=1)), axis=1)
+        plt.plot(plotted_array[:,0], plotted_array[:,1], markertype, label=f"Circuit {part_no}", markersize=2)
+    
     plt.title("Puissance moyenne dissipée [W] par la charge en fonction de la résistance [$\Omega$]")
     plt.xlabel("Résistance totale des composantes du circuit [$\Omega$]")
     plt.ylabel("Puissance moyenne dissipée [W]")
@@ -98,6 +135,4 @@ def make_power_figure():
     plt.legend(loc="upper right")
     plt.show()
 
-
-
-make_power_figure()
+make_filix_figure()
