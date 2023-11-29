81bb78dab27b9e0fa407580980890730f4b6a2de
FelixDesroches <117761543+FelixDesroches@users.noreply.github.com>
2023-11-28 20:54:57 -0500

    added circuit 2

--- homework_2/graphs.py
+++ homework_2/graphs.py
@@ -2,6 +2,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+def gain_equation_2(omega):
+    s = 1j*omega
+    return np.abs(
+        (135*s*10**6)/(46*10**12+546*s*10**6+1620*s**2)
+    )
+    # EQUATION WITHOUT j
+    # return (
+    #     (((-omega**2+10**9)*(-omega**2*(1+270*10**(-3))+10**9+270*10**6))/((-omega**2*(1+270*10**(-3))+10**9+270*10**6)**2
+    #                                                                      +(270*10**3*omega)**2))**2
+    #         +((270*10**3*omega)/((-omega**2*(1+270*10**(-3))+10**9+270*10**6)**2+(270*10**3*omega)**2))**2)**(1/2)
+
+
 def gain_equation_1(omega):
     s = 1j*omega
     return np.abs(
@@ -12,6 +24,7 @@ def gain_equation_1(omega):
     #     (((-omega**2+10**9)*(-omega**2*(1+270*10**(-3))+10**9+270*10**6))/((-omega**2*(1+270*10**(-3))+10**9+270*10**6)**2
     #                                                                      +(270*10**3*omega)**2))**2
     #         +((270*10**3*omega)/((-omega**2*(1+270*10**(-3))+10**9+270*10**6)**2+(270*10**3*omega)**2))**2)**(1/2)
+
 
 def phase_equation_1(omega):
     s = 1j*omega
@@ -53,4 +66,24 @@ def circuit_1():
     plt.xscale("log")
     plt.show()
 
-circuit_1()
+
+def circuit_2():
+    omega_space = np.logspace(1, 8, 20000, base=10)
+
+    # max_P = np.abs(((1j*10)**2+10**9)/((1j*10)**2*(1+270*10**(-3))+270*10**3*(1j*10)+10**9+270*10**6))
+    # line_y = 10**(-3/10)*max_P
+    # intersections = omega_space[np.isclose(gain_equation_2(omega_space), np.full(20000,line_y), rtol=0.0001)]
+    # plt.plot(intersections, [line_y, line_y], "go")
+    # plt.text(intersections[0]/(10**1.6), line_y+0.04, (rf"$\omega$={intersections[0]:.2e} " + r"$s^{-1}$"), fontsize=9)
+    # plt.text(intersections[1]*(10**0.2), line_y-0.04, (rf"$\omega$={intersections[1]:.2e} " + r"$s^{-1}$"), fontsize=9)
+
+    plt.plot(omega_space, gain_equation_2(omega_space), "mo", markersize=2)
+    # plt.plot([10,10**7], [line_y, line_y], "b-")
+    plt.xlabel(r"Fréquence angulaire $\omega$ [$s^{-1}$]")
+    plt.ylabel(r"Gain $G$ [-]")
+    plt.xscale("log")
+    plt.show()
+
+
+circuit_2()
+# circuit_1()
